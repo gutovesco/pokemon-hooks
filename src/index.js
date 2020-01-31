ef2805f990f2c36580e3ea764df344b71ecb2700
gutovesco <guto.vesco@hotmail.com>
2020-01-30 23:28:34 -0300

    setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+export default function App() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
